be17bec9e9fed72caee31b69a77329572d1f063d
stevenlegere <stevenmarklegere@gmail.com>
2023-08-07 08:07:53 +0100

    Comments added

--- my-app/src/components/SmoothieCard.tsx
+++ my-app/src/components/SmoothieCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../index.css';
-
+// Interface defining the structure of a Smoothie object
 interface Smoothie {
     id: number;
     title: string;
@@ -8,14 +8,15 @@ interface Smoothie {
     allergen: string[];
     ingredients: string[];
 }
-
+// Interface defining the properties for the SmoothieCard component
 interface SmoothieCardProps {
     smoothie: Smoothie;
 }
 
-
+// SmoothieCard component to display information about a smoothie
 const SmoothieCard: React.FC<SmoothieCardProps> = ( { smoothie }) => {
     return (
+        // Container for the smoothie card with the "smoothie-card" class
         <div className="smoothie-card">
             <h3>{smoothie.title}</h3>
             <p>{smoothie.allergen.join(', ')}</p>
--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -5,7 +5,7 @@ import '../index.css';
 import NavBar from '../components/NavBar';
 import SmoothieCard from '../components/SmoothieCard';
 
-
+// Interface defining the structure of a Smoothie object
 interface Smoothie {
     id: number;
     title: string;
@@ -13,20 +13,22 @@ interface Smoothie {
     allergen: string[];
     ingredients: string[];
 }
-
+// Home component displaying a list of smoothies and an allergen filter
 const Home: React.FC = () => {
     <NavBar />
     console.log(supabase);
+    // State variables to handle smoothie data and allergen filtering
     const [fetchError, setFetchError] = useState<string | null>(null);
     const [smoothies, setSmoothies] = useState<Smoothie[] | null>(null);
     const [selectedAllergens, setSelectedAllergens] = useState<string[]>([]);
 
+    // useEffect hook to fetch smoothies from the database on component mount
     useEffect(() => {
         const fetchSmoothies = async () => {
             const { data, error } = await supabase
                 .from('smoothies')
                 .select()
-
+    // Handle any fetch errors and update state accordingly
             if (error) {
                 setFetchError('Could not fetch the smoothies')
                 setSmoothies(null)
@@ -37,17 +39,18 @@ const Home: React.FC = () => {
                 setFetchError(null)
             }
         }
-
+        // Invoke the fetchSmoothies function
         fetchSmoothies()
 
     }, [])
 
+    // Function to handle allergen selection and filtering
     const handleAllergensChange = (allergen: string) => {
         setSelectedAllergens((prev) =>
             prev.includes(allergen) ? prev.filter((item) => item !== allergen) : [...prev, allergen]
         );
     };
-
+    // Render the Home component with smoothie data and allergen filter
     return (
         <div className="page home">
           {fetchError && <p>{fetchError}</p>}
